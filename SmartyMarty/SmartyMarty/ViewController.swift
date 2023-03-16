13558e9ca71d30deea16e31cd054ba1c883e187c
afanasiev-kirill <kirillafanasiev@ukr.net>
2023-03-16 16:27:00 -0400

    starting project

---
+++ SmartyMarty/SmartyMarty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartyMarty
+//
+//  Created by Kirill Afanasiev on 2023-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
